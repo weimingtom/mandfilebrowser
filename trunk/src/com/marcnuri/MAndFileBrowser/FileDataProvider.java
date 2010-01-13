f6c09da0b5d3f17192b8611206abf324d07852c3
marcnuri <marcnuri@2172c4a2-fa07-11de-91aa-85f76f42a988>
2010-01-13 07:47:46 +0000

    Added icons for folder, audio, image, up.

--- trunk/src/com/marcnuri/MAndFileBrowser/FileDataProvider.java
+++ trunk/src/com/marcnuri/MAndFileBrowser/FileDataProvider.java
@@ -7,15 +7,18 @@ import java.util.Comparator;
 import java.util.HashMap;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.widget.SimpleAdapter;
 
 public class FileDataProvider {
 
 	private final static String MAP_KEY_ICON = "MAP_KEY_ICON";
 	private final static String MAP_KEY_FILE_NAME = "MAP_KEY_FILE_NAME";
+	private final static String MAP_KEY_FILE_SIZE = "MAP_KEY_FILE_SIZE";
 	private final static String MAP_ABSOLUTE_PATH = "MAP_ABSOLUTE_PATH";
 	private final static String MAP_KEY_FILE = "MAP_KEY_FILE";
 	
+	private Context context;
 	private File currentDirectory; 
 	private Comparator<File> comparator;
 	private ArrayList<HashMap<String, Object>> list;
@@ -26,7 +29,7 @@ public class FileDataProvider {
 	 *
 	 */
 	public FileDataProvider(Context context) {
-		System.out.println("ADAPTER CREATED");
+		this.context = context;
 		currentDirectory = new File("/");
 		comparator = new FileComparator();
 		list = new ArrayList<HashMap<String, Object>>();
@@ -46,39 +49,67 @@ public class FileDataProvider {
 		navigateTo((File)list.get(position).get(MAP_KEY_FILE));
 	}
 	private void navigateTo(File f){
-		if(!f.isDirectory()){
+		if(f == null){
+			up();
+		}
+		else if(!f.isDirectory()){
+			//TODO: Code to open Files
 			return;
-		}
-		list.clear();
-		currentDirectory = f;
-		File[] files = f.listFiles();
-		Arrays.sort(files, comparator);
-		int length = files.length;
-		int folder = R.drawable.folder;
-		int text = R.drawable.text;
-		for(int it = 0; it < length; it++){
-			HashMap<String, Object> map = new HashMap<String, Object>();
-			File fTemp = files[it];
-			map.put(MAP_KEY_ICON, getIconForFile(fTemp));
-			if(fTemp.isDirectory()){
-				map.put(MAP_KEY_ICON, folder);
-			} else {
-				map.put(MAP_KEY_ICON, text);
-			}
-			map.put(MAP_KEY_FILE_NAME, fTemp.getName());
-			map.put(MAP_ABSOLUTE_PATH, fTemp.getAbsolutePath());
-			map.put(MAP_KEY_FILE, fTemp);
-			list.add(map);
-		}
-		adapter.notifyDataSetChanged();
-	}
-	public int getIconForFile(File f){
-		int ret = -1;
-		if(f.isDirectory()){
-			ret = R.drawable.folder;
 		} else {
-			 ret = R.drawable.text;
-			 
+			list.clear();
+			currentDirectory = f;
+			//Put up icon
+			if(!f.getAbsolutePath().equals("/")){
+				list.add(getHashMapForFile(null));
+			}
+			File[] files = f.listFiles();
+			Arrays.sort(files, comparator);
+			int length = files.length;
+			for(int it = 0; it < length; it++){
+				list.add(getHashMapForFile(files[it]));
+			}
+			adapter.notifyDataSetChanged();
+		}
+	}
+	private HashMap<String, Object> getHashMapForFile(File file){
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		map.put(MAP_KEY_FILE, file);
+		map.put(MAP_KEY_ICON, getIconForFile(file));
+			if(file != null){
+			map.put(MAP_KEY_FILE_NAME, file.getName());
+			map.put(MAP_KEY_FILE_SIZE, file.length());
+			map.put(MAP_ABSOLUTE_PATH, file.getAbsolutePath());
+		} else {
+			map.put(MAP_KEY_FILE_NAME, "..");
+			map.put(MAP_KEY_FILE_SIZE, 0);
+			map.put(MAP_ABSOLUTE_PATH, "");
+		}
+		return map;		
+	}
+	private int getIconForFile(File f){
+		int ret = -1;
+		if(f ==null){
+			ret = R.drawable.iconup;
+		} else if(f.isDirectory()){
+			ret = R.drawable.iconfolder;
+		} else {
+			 ret = R.drawable.icontext;
+			 String fileName = f.getName();
+			 Resources resources = context.getResources();
+			 if(isExtensionInArray(fileName, resources.getStringArray(R.array.audio))){
+				 ret = R.drawable.iconaudio;
+			 } else if(isExtensionInArray(fileName, resources.getStringArray(R.array.image))){
+				 ret = R.drawable.iconimage;
+			 } 
+		}
+		return ret;
+	}
+	private boolean isExtensionInArray(String fileName, String[] array){
+		boolean ret = false;
+		for(String extension : array){
+			if(fileName.endsWith("."+extension)){
+				ret = true;
+			}
 		}
 		return ret;
 	}
